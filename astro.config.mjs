91ec036b95a86432b284b9502316899727de8dfb
Ryan Won <ryanwon7@gmail.com>
2025-11-10 00:25:02 -0500

    more support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Replace with your site URL
+  site: 'https://ryanwon7.github.io', // Replace with your site URL
+  base: '/lee-tkma',
   integrations: [
     tailwind(),
     sitemap(),
